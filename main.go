eaa8ae6568809410faad31adc0da79bc353a03bf
Josh Albrecht <joshmalbrecht@gmail.com>
2025-05-07 14:41:20 -0400

    Fixed issue with version command not working
    - Updated the version command to read from the main.version variablea
      that is defined in the binary by using the ldflags.

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/joshmalbrecht/note/cmd"
+import (
+	"github.com/joshmalbrecht/note/cmd"
+	"github.com/joshmalbrecht/note/internal/config"
+)
+
+var (
+	version = "devel"
+	commit  = "none"
+	date    = "unknown"
+)
 
 func main() {
+	config.Version = version
+	config.Commit = commit
+	config.Date = date
+
 	cmd.Execute()
 }
